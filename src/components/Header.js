19a5089eb9842f72fbd940bd6ba686b4cbeb0167
evanyang0612 <blacksheet@gmail.com>
2022-10-18 11:05:32 +0800

    bug fixed 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import trollFace from "../images/troll-face.webp";
 export default function Header() {
   return (
     <header className="header">
-      <img src={trollFace} className="header--image" />
+      <img src={trollFace} className="header--image" alt="troll face" />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">React Project by Evan Yang</h4>
     </header>
